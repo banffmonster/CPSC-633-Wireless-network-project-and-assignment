454d8238efda304bfc462303aaf584a46ec785b1
lei wang <44036753+banffmonster@users.noreply.github.com>
2018-10-10 13:08:52 -0600

    Update assignment1_plot_of_throughput.m

--- assignment1_plot_of_throughput.m
+++ assignment1_plot_of_throughput.m
@@ -1,15 +1,17 @@
-d = 2;
-r = 1;
-lambda = 1;
-theta = 1;
-alpha = 4;
-cd = pi;
-delta = d/alpha;
-p = 0: 0.001:1;
-Ps = exp((-cd*lambda*p*(r^d)*(theta^delta))./(sinc(delta)));
-Psh = exp((-cd*lambda*p.*(1-p)*(r^d)*(theta^delta))./(sinc(delta)));
+%analytical curves of throughput T on transmitt probability p in a PPP wireless network, T = p(1-p)Ps(p) 
+%solution for problem 5.5 in <<Stochastic Geometry for Wireless Networks by Martin Haenggi, Cambrige Press, 2013>>
+d = 2;%demension
+r = 1;%distance between the desired tranmitter and the receiver
+lambda = 1;%insensity of the nodes
+theta = 1;%SINR threshold for acceptable communication
+alpha = 4;%path loss exponent
+cd = pi;%pi
+delta = d/alpha;%characteristic exponent
+p = 0: 0.001:1;%transmitt probability
+Ps = exp((-cd*lambda*p*(r^d)*(theta^delta))./(sinc(delta)));%success probability for hal-duplex PPP
+Psh = exp((-cd*lambda*p.*(1-p)*(r^d)*(theta^delta))./(sinc(delta)));%success probability for full-duplex PPP
 a1 = subplot(2,1,1);
-T = p.*(1-p).*Ps; 
+T = p.*(1-p).*Ps; $throughtput in problem 5.5
 figure(1)
 plot(p, T)
 title('half-duplex')
