aca8f029354e3fd6da44f14933b7a5c23907badd
lei wang <44036753+banffmonster@users.noreply.github.com>
2018-10-10 13:47:02 -0600

    Update assignment1_simulation1_fading_inferers.m

--- assignment1_simulation1_fading_inferers.m
+++ assignment1_simulation1_fading_inferers.m
@@ -1,3 +1,4 @@
+%simulation of success probability Ps on transmitt probability p with Rayleigh fading interferers 
 cd = pi;
 I = 0;
 D = 2;
@@ -6,39 +7,40 @@ lambda = 1;
 theta = 1;
 alpha = 4;
 delta = D/alpha;
-R = 32;
+R = 32;%radius of the PPP
 ctr =0;
 pt =1;
 Ps = [];
 p = [];
-for p = 0.05: 0.05 : 0.95
+for p = 0.05: 0.05 : 0.95%increase of transmitt probability from 0.05 to 0.95
     p = p;
     ctr = 0;
-    for nt = 1:1:100
-        N1 = poissrnd(lambda*pi*R^2);
-        UE = unifrnd(-R, R, N1, 2);
+    for nt = 1:1:100%iteration times
+        N1 = poissrnd(lambda*pi*R^2);%generate the interferer number of PPP
+        UE = unifrnd(-R, R, N1, 2);%generate the coordinates 
         x = UE(:,1);
         y = UE(:,2);
-        d = sqrt(x.^2+y.^2);
-        for nf = 1:1:10
+        d = sqrt(x.^2+y.^2);%distance between every undesired transmitter and the receiver
+        for nf = 1:1:10%choose different senders
             I = 0;
-            S = 0;
-            Si = 0;
-            SIR = 0;
-            pf = rand(1);
+            S = 0;%desired signal power
+            Si = 0;%undesired signal power
+            SIR = 0;%Signal-to-interference-ratio
+            pf = rand(1);%random value of interfer node to compare with the probability p
             if(pf > p)
                     hi = exprnd(1);
                     for n1 = 1:1:N1
                         pi = rand(1);
                         if (pi > p)
-                           S = pt*exprnd(1)*((d(n1))^-alpha);
-                           I = I+S;
+                           Si = pt*exprnd(1)*((d(n1))^-alpha);%calculation of undesired signal power,exprnd(1) is the fading function
+                                                                %pt is the transmit power
+                           I = I+Si;
                         end
                     end 
-                    Si = pt*hi*(r^-alpha);
-                    SIR = Si/I;
+                    S = pt*hi*(r^-alpha);%calculation of the desired signal power at the receiving node
+                    SIR = S/I;
                     if SIR > theta
-                        ctr = ctr + 1;
+                        ctr = ctr + 1;%count the times that the transmitting is successful
                     end
             end
             
